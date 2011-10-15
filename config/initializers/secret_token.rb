e67e7aacde8e0a24c0707e63088097f6be451ccc
Jamie Macey <jamie@tracefunc.com>
2011-10-15 11:23:50 -0700

    rails new househunter

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Househunter::Application.config.secret_token = 'c455ef54e2f3049a0a84b9ca0aa4edb43fa0777440ba45be9c3264c3590dfc4450a17b5ee93f38088ccc3c333b5a5f8d67e3ea41ba49c8d27d71dec72115a054'
