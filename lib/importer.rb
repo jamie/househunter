3b8bb396a977874e9c2958a3e1ebc0b9e5557e72
Jamie Macey <jamie.git@tracefunc.com>
2024-04-21 14:48:55 -0700

    Standardrb

--- lib/importer.rb
+++ lib/importer.rb
@@ -1,17 +1,17 @@
 class Importer
   def do_import
-    require 'builder'
-    require 'cgi'
-    require 'json'
-    require 'open-uri'
+    require "builder"
+    require "cgi"
+    require "json"
+    require "open-uri"
 
     7.times do |i|
-      xml = xml_for_price(i*50000, (i+1)*50000)
-      url = "http://www.realtor.ca/handlers/MapSearchHandler.ashx?xml=#{CGI::escape(xml).gsub('%2F','/')}"
+      xml = xml_for_price(i * 50000, (i + 1) * 50000)
+      url = "http://www.realtor.ca/handlers/MapSearchHandler.ashx?xml=#{CGI.escape(xml).gsub("%2F", "/")}"
 
       json = JSON.parse(open(url).read)
-      next if json['MapSearchResults'].blank?
-      json['MapSearchResults'].each do |listing_json|
+      next if json["MapSearchResults"].blank?
+      json["MapSearchResults"].each do |listing_json|
         Listing.import(listing_json)
       end
     end
@@ -19,14 +19,14 @@ class Importer
 
   def xml_for_price(min, max)
     Builder::XmlMarkup.new.ListingSearchMap do |x|
-      x.Culture 'en-CA'
-      x.OrderBy '1'
-      x.OrderDirection 'A'
-      x.Culture 'en-CA'
-      x.LatitudeMax '49.33'
-      x.LatitudeMin '49.03'
-      x.LongitudeMax '-123.85'
-      x.LongitudeMin '-124.08'
+      x.Culture "en-CA"
+      x.OrderBy "1"
+      x.OrderDirection "A"
+      x.Culture "en-CA"
+      x.LatitudeMax "49.33"
+      x.LatitudeMin "49.03"
+      x.LongitudeMax "-123.85"
+      x.LongitudeMin "-124.08"
       x.PriceMax max
       x.PriceMin min
       x.PropertyTypeID 300
--- lib/listing.rb
+++ lib/listing.rb
@@ -1,15 +1,15 @@
 class Hash
   def diff(h2)
-    self.dup.delete_if { |k, v| h2[k] == v }.merge(h2.dup.delete_if { |k, v| self.has_key?(k) })
+    dup.delete_if { |k, v| h2[k] == v }.merge(h2.dup.delete_if { |k, v| has_key?(k) })
   end
 end
 
 class Listing < ActiveRecord::Base
-  IGNORED_ATTRS = %w(PropertyImagePath PropertyLowResImagePath)
-  UNIMPORTANT_ATTRS = %w(PropertyImagePath PropertyLowResImagePath PropertyLowResPhotos Latitude Longitude OrganizationName)
-  
+  IGNORED_ATTRS = %w[PropertyImagePath PropertyLowResImagePath]
+  UNIMPORTANT_ATTRS = %w[PropertyImagePath PropertyLowResImagePath PropertyLowResPhotos Latitude Longitude OrganizationName]
+
   def self.filtered
-    where("status != ?", 'ignore')
+    where("status != ?", "ignore")
   end
 
   def self.recent
@@ -17,8 +17,8 @@ class Listing < ActiveRecord::Base
   end
 
   def self.import(attrs)
-    listing = where(:mls => attrs['MLS']).first
-    listing ||= create(:mls => attrs['MLS'])
+    listing = where(mls: attrs["MLS"]).first
+    listing ||= create(mls: attrs["MLS"])
     listing.record(attrs)
     listing.import!
   end
@@ -27,14 +27,14 @@ class Listing < ActiveRecord::Base
     return if last_imported == attrs
     return if ((last_imported.diff attrs).keys - IGNORED_ATTRS).empty?
     if json.blank?
-      print '*'
+      print "*"
       self.json = attrs.to_json
       self.imported_at = Time.now
     else
-      print '.'
-      p (last_imported.diff attrs)
-      p (attrs.diff last_imported)
-      self.json = self.json + "\n" + attrs.to_json
+      print "."
+      p(last_imported.diff(attrs))
+      p(attrs.diff(last_imported))
+      self.json = json + "\n" + attrs.to_json
       if !((last_imported.diff attrs).keys - UNIMPORTANT_ATTRS).empty?
         self.imported_at = Time.now
       end
@@ -43,41 +43,41 @@ class Listing < ActiveRecord::Base
   end
 
   def last_imported
-    JSON.parse((json||"").split("\n").last || "{}")
+    JSON.parse((json || "").split("\n").last || "{}")
   end
 
   def import!
     a = last_imported
-    self.lat =     a['Latitude']
-    self.lng =     a['Longitude']
-    self.address = a['Address'].blank? ? 'Unknown' : a['Address']
-    self.price =   a['Price']
+    self.lat = a["Latitude"]
+    self.lng = a["Longitude"]
+    self.address = a["Address"].blank? ? "Unknown" : a["Address"]
+    self.price = a["Price"]
     save
   end
-  
-  def map_icon_uri
-    return "/starpin.png" if status == 'remember'
 
-    color = case
-    when price_int < 180_000; 'pink'
-    when price_int < 210_000; 'purple'
-    when price_int < 240_000; 'blue'
-    when price_int < 270_000; 'green'
-    when price_int < 300_000; 'yellow'
-    when price_int < 330_000; 'orange'
-    else                    ; 'red'
+  def map_icon_uri
+    return "/starpin.png" if status == "remember"
+
+    color = if price_int < 180_000 then "pink"
+    elsif price_int < 210_000 then "purple"
+    elsif price_int < 240_000 then "blue"
+    elsif price_int < 270_000 then "green"
+    elsif price_int < 300_000 then "yellow"
+    elsif price_int < 330_000 then "orange"
+    else
+      "red"
     end
-    style = (imported_at > 20.hours.ago ? '-dot' : '')
+    style = ((imported_at > 20.hours.ago) ? "-dot" : "")
     "http://maps.google.com/mapfiles/ms/icons/#{color}#{style}.png"
   end
-  
+
   def price_int
-    price.gsub(/[^0-9]/, '').to_i
+    price.gsub(/[^0-9]/, "").to_i
   end
 
   def url
-    id = last_imported['PropertyID']
-    key = last_imported['PidKey']
+    id = last_imported["PropertyID"]
+    key = last_imported["PidKey"]
     "http://www.realtor.ca/propertyDetails.aspx?propertyId=#{id}&PidKey=#{key}"
   end
 end
