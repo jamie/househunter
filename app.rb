cc45de0e718dbe930f029c08bf931de2bed0db90
Jamie Macey <jamie.git@tracefunc.com>
2024-04-21 14:36:33 -0700

    Standardrb

--- app.rb
+++ app.rb
@@ -1,34 +1,34 @@
-require './setup'
+require "./setup"
 
-require 'sinatra'
-require 'cgi'
-require 'open-uri'
+require "sinatra"
+require "cgi"
+require "open-uri"
 
-get '/' do
+get "/" do
   @listings = Listing.recent.filtered.all
   erb :index
 end
 
-get '/all' do
+get "/all" do
   @listings = Listing.recent.all
   erb :index
 end
 
-get '/remembered' do
-  @listings = Listing.where(:status => 'remember').all
+get "/remembered" do
+  @listings = Listing.where(status: "remember").all
   erb :index
 end
 
-get '/listing/:id/mls' do
+get "/listing/:id/mls" do
   listing = Listing.find(params[:id])
-  out = open(listing.url).read
+  out = URI.open(listing.url).read
   out.sub!("<SCRIPT LANGUAGE='Javascript'>if (window != top) top.location.href = location.href;</SCRIPT>", "")
   out.sub!('"REALTOR">', '"REALTOR"><base href="http://www.realtor.ca/">')
   out
 end
 
-post '/listing/:id/:status' do
-  pass unless %w(new ignore remember).include? params[:status]
+post "/listing/:id/:status" do
+  pass unless %w[new ignore remember].include? params[:status]
   listing = Listing.find(params[:id])
   listing.status = params[:status]
   listing.save
@@ -76,7 +76,7 @@ __END__
           icon: '<%= listing.map_icon_uri %>',
           title: "<%= listing.address %>"
         });
-        
+
         var infoWindow = new google.maps.InfoWindow({content: ""});
 
         google.maps.event.addListener(markers[<%= listing.id %>], 'click', function() {
@@ -148,4 +148,3 @@ __END__
   <% end %>
 </body>
 </html>
-
--- setup.rb
+++ setup.rb
@@ -1,20 +1,20 @@
-require 'rubygems'
-require 'bundler/setup'
+require "rubygems"
+require "bundler/setup"
 
-require 'active_record'
+require "active_record"
 
-if ENV['RACK_ENV'] == 'production'
-  ENV['DATABASE_URL'] =~ %r{^postgres://(.*):(.*)@(.*)/(.*)$}
+if ENV["RACK_ENV"] == "production"
+  ENV["DATABASE_URL"] =~ %r{^postgres://(.*):(.*)@(.*)/(.*)$}
   ActiveRecord::Base.establish_connection(
-    :adapter  => 'postgresql',
-    :username => $1,
-    :password => $2,
-    :host     => $3,
-    :database => $4
+    adapter: "postgresql",
+    username: $1,
+    password: $2,
+    host: $3,
+    database: $4
   )
 else
-  ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database => 'db/development.sqlite3')
+  ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: "db/development.sqlite3")
 end
 
-require './lib/listing'
-require './lib/importer'
+require "./lib/listing"
+require "./lib/importer"
