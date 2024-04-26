3f91a5797037e89b7fd5ef15fec4677d0923ef41
Jamie Macey <jamie.git@tracefunc.com>
2024-04-25 18:06:41 -0700

    Improve marker codegen

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,2 @@
 module ApplicationHelper
-  def leaflet_icon_definition(icon_url)
-    <<~JS.html_safe
-      #{icon_url}: L.icon({
-        iconUrl: '#{image_path(icon_url)}',
-        iconSize: [20,20],
-        iconAnchor: [10,10]
-      })
-    JS
-  end
 end
