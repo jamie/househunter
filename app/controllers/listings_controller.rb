60458751f724dcbcf171eddb4a6246285a8511d5
Jamie Macey <jamie.git@tracefunc.com>
2024-04-26 21:46:08 -0700

    Externalize map marker loading/assignment

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,7 +1,14 @@
 class ListingsController < ApplicationController
+  skip_forgery_protection
+
   def index
     relation = Listing.max_price(nil).filtered.recent
     @price_spread = relation.price_spread
     @listings = relation.all
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end
