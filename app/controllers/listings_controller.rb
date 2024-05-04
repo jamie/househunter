ea7ad701f128363a774787988c783c1184bb88aa
Jamie Macey <jamie.git@tracefunc.com>
2024-05-03 22:48:27 -0700

    Switch to manual two-headed price range input

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,10 +1,17 @@
 class ListingsController < ApplicationController
+  UNLIMITED_PRICE = 2_250_000
+
   skip_forgery_protection
 
   def index
     ImportJob.perform_later if Listing.maximum(:created_at) < 8.hours.ago
+    session[:min_price] = params[:min_price] if params[:min_price].present?
+    session[:max_price] = params[:max_price] if params[:max_price].present?
 
-    relation = Listing.min_price(params[:min_price]).max_price(params[:max_price]).filtered.recent
+    @min_price = session[:min_price] || 0
+    @max_price = session[:max_price] || UNLIMITED_PRICE
+
+    relation = Listing.min_price(@min_price).max_price(@max_price).filtered.recent
     @price_spread = relation.price_spread
     @listings = relation.all
 
